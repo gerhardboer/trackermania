db72dc3e5f868b2314b154048135768703ab6f8b
Gerhard <gerhard@reconcept.nl>
2023-11-25 21:03:32 +0100

    basic tier list

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TierListComponent } from './tier-list.component';
 
 @Component({
   selector: 'trm-main',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>main works!</p>`,
+  imports: [CommonModule, TierListComponent],
+  template: `
+    <p>main works!</p>
+    <trm-tier-list></trm-tier-list>
+  `,
   styles: ``,
 })
 export class MainComponent {}
