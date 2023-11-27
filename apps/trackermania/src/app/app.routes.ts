6c62df239da63eda8c15a5bfc06318be9410182e
Gerhard <gerhard@reconcept.nl>
2023-11-27 21:58:05 +0100

    add simple login

--- apps/trackermania/src/app/app.routes.ts
+++ apps/trackermania/src/app/app.routes.ts
@@ -3,7 +3,11 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: '',
-    pathMatch: 'full',
+    loadComponent: () =>
+      import('./login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'tierlist',
     loadComponent: () =>
       import('./main/main.component').then((m) => m.MainComponent),
   },
---
+++ apps/trackermania/src/app/login.component.ts
@@ -0,0 +1,43 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'trm-login',
+  template: `
+    <div class="login">
+      <div class="login__container">
+        <div class="login__form">
+          <h1>Login</h1>
+          <div class="login__form__input">
+            <input
+              type="text"
+              placeholder="Username"
+              ngModel
+              #username="ngModel"
+            />
+          </div>
+          <div class="login__form__button">
+            <button
+              (click)="login(username.value)"
+              [disabled]="!username.value"
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  `,
+  standalone: true,
+  styleUrl: './login.component.scss',
+  imports: [FormsModule],
+})
+export class LoginComponent {
+  private router = inject(Router);
+
+  login(username: string) {
+    localStorage.setItem('user', username);
+    this.router.navigate(['/tierlist']);
+  }
+}
