5c46ee9de88881591a92b36d4cb7a867bf50e6bd
Gerhard <gerhard@reconcept.nl>
2024-01-01 21:48:43 +0100

    improve showing times

--- apps/trackermania/src/app/main/times.component.ts
+++ apps/trackermania/src/app/main/times.component.ts
@@ -12,7 +12,7 @@ import { Campaign, Map, Time } from '../types';
     <section class="content">
       <section class="content-title">
         <header>
-          <span>Times</span>
+          <span>Your stats</span>
           <button
             class="button header-button content-title__header-button"
             (click)="dialogElement.showModal()"
@@ -76,6 +76,24 @@ export class TimesComponent {
         season: 'Spring',
         year: '2023',
         maps: [
+          {
+            name: 'Spring 2023 - 04',
+            author: 'Nadeo',
+            url: 'https://core.trackmania.nadeo.live/storageObjects/7feb0c6c-bbc5-4fc8-b5b4-713b423de1f9',
+            thumbnail:
+              'https://core.trackmania.nadeo.live/storageObjects/b1f77f37-e855-4e12-a3bd-13731c3163e8.jpg',
+            uploaded: '2023-03-23T11:12:13.000Z',
+            storageId: 'b1f77f37-e855-4e12-a3bd-13731c3163e8',
+            uid: 'GYC5gFrd3TvWuyHRWe37ManmC76',
+            fileName: 'Spring 2023 - 04.Map.Gbx',
+            submitterName: 'Nadeo',
+            time: {
+              h: 1,
+              mm: 1,
+              ss: 1,
+              SSS: 1,
+            },
+          },
           {
             name: 'Spring 2023 - 04',
             author: 'Nadeo',
